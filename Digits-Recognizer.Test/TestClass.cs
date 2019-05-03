6ab3c6cdedb087003449d78683fa879287274010
Anđela Todorović <andjelatodorovicc@gmail.com>
2019-05-03 20:52:42 +0200

    Add files via upload

---
+++ Digits-Recognizer.Test/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Digits_Recognizer.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Test()
+        {
+            var x = Beginning.beginning.distance(new[] {1, 2}, new[] {1, 2});
+            Assert.That(x, Is.EqualTo(0));
+        } 
+        
+        [Test]
+        public void Test2()
+        {
+            var x = Beginning.beginning.distance(new[] {2, 4}, new[] {1, 2});
+            Assert.That(x, Is.EqualTo(Math.Sqrt(5)));
+        }
+    }
+}
